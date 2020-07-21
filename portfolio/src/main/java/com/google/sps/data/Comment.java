dc9beb7d9738e45b0fae080b21f52d58ddcabebf
Antonia <bocaantonia@gmail.com>
2020-07-21 17:16:01 +0300

    add Comment class

---
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -0,0 +1,25 @@
+package com.google.sps.data;
+
+public class Comment {
+    private final String name;
+    private final String text;
+    private final Long id;
+
+    Comment(String name, String text, Long id) {
+        this.name = name;
+        this.text = text;
+        this.id = id;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    String getText() {
+        return this.text;
+    }
+
+    Long getID() {
+        return this.id;
+    }
+}
