40150879d48c749faf8f7219be8c05cd82ac8071
Antonia <bocaantonia@gmail.com>
2020-07-27 15:45:06 +0300

    add gogle sign in button

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -105,3 +105,11 @@ function createCommentNode(comment) {
 
   return comDiv;
 }
+
+function onSignIn(googleUser) {
+  const profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
