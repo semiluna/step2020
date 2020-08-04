8d3e718bd3b799ba2a63034a4d0d7ad51002d2a4
Antonia <bocaantonia@gmail.com>
2020-08-04 11:43:36 +0300

    implement query to pass given tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,89 @@
 
 package com.google.sps;
 
+import java.util.List;
+import java.util.Collections;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import com.google.sps.TimeRange;
 
 public final class FindMeetingQuery {
+  private final static int totalMinutes = 1440;
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    //remove events with no requested attendees
+    HashSet<String> requested = new HashSet<>(request.getAttendees());
+    HashSet<Event> eventsSet = new HashSet<>(events);
+
+    ArrayList<TimeRange> list = eventsSet
+      .stream()
+      .filter(event -> this.checkAttendees(event, request) == true)
+      .map(event -> event.getWhen())
+      .collect(Collectors.toCollection(ArrayList::new));
+
+    ArrayList<TimeRange> times = new ArrayList<>(list);
+    System.out.println(times);
+
+    //sort time ranges by start
+    Collections.sort(times, TimeRange.ORDER_BY_START);
+
+    //get free times
+    ArrayList<TimeRange> freeTimes = new ArrayList();
+    
+    if (times.isEmpty() && (request.getDuration() <= totalMinutes)) {
+      freeTimes.add(TimeRange.WHOLE_DAY);
+    }
+
+    if (times.isEmpty()) {
+      return freeTimes;
+    }
+
+    TimeRange intersection = times.get(0);
+
+    for (TimeRange range: times) {
+      if (intersection.contains(range)) {
+        continue;
+      } else {
+        if (intersection.overlaps(range)) {
+          intersection = new TimeRange(intersection.start(), range.end() - intersection.start());
+        } else {
+          //found free time
+          TimeRange free = new TimeRange(intersection.end(), range.start() - intersection.end());
+          intersection = new TimeRange(range.start(), range.end() - range.start());
+          if (free.duration() >= request.getDuration()) {
+            freeTimes.add(free);
+          }
+        }
+      }
+    }
+
+    //check for beginning and end
+    if (TimeRange.START_OF_DAY < times.get(0).start()) {
+      freeTimes.add(new TimeRange(0, times.get(0).start()));
+    }
+
+    if (intersection.end() < TimeRange.END_OF_DAY) {
+      int end = intersection.end();
+      freeTimes.add(new TimeRange(end, totalMinutes - end));
+    }
+    Collections.sort(freeTimes, TimeRange.ORDER_BY_START);
+
+    return freeTimes;
+  }
+
+  private static boolean checkAttendees(Event event, MeetingRequest request) {
+    HashSet<String> attendees = new HashSet<>(event.getAttendees());
+    HashSet<String> requested = new HashSet<>(request.getAttendees());
+
+    for (String person: requested) {
+      if (attendees.contains(person)) {
+        return true;
+      }
+    }
+    
+    return false;
   }
 }
